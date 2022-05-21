d2a8d8de2fa006dc664de26ef2a82f83f04f2649
Balu Shrama <69688211+Balanjaneyasharma@users.noreply.github.com>
2022-05-21 20:03:38 +0530

    Update AddStrings.cpp

--- AddStrings.cpp
+++ AddStrings.cpp
@@ -1,51 +1,34 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int n1=num1.size()-1,n2=num2.size()-1;
-        int carry=0;
-        string ans="";
-        while(n1>=0&&n2>=0){
-            int digit = (num1[n1]-'0') + (num2[n2]-'0') + carry;
-            int rem=digit%10;
-            char c ='0' + rem ;
-            ans=c+ans;
-            carry=digit/10;
-            n1--;
-            n2--;
+        if(num1.size() < num2.size()) swap(num1,num2);
+        int n1 = num1.size() ; 
+        int n2 = num2.size() ;
+        
+        string ans = "";
+        int sum ,carry = 0;
+        char c ;
+        while(n1 != 0){
+            if(n2 != 0){
+                sum = num1[n1-1] - '0' + num2[n2-1] - '0' + carry ;
+                c = '0' + (sum%10);
+                ans = c + ans ;
+                if(sum >= 10) carry = 1;
+                else carry = 0;
+                n2 --;
+            }
+            else{ 
+                sum = num1[n1-1]-'0' + carry ;
+                c = '0' + (sum%10);
+                ans = c + ans ;
+                if(sum >= 10) carry = 1;
+                else carry = 0;
+            }
+            n1 --;
         }
-        cout<<n1<<n2<<endl;
-        if(n1==n2&&carry>0){
-            char t= '0'+carry;
-            ans=t+ans;
-            carry=0;
-        }
-        else if(n1>=0){
-            while(n1>=0){
-                int d = num1[n1]-'0'+ carry;
-                int y =d%10;
-                char r = '0' + y;
-                ans= r+ans;
-                carry=d/10;
-                n1--;
-            }
-            if(carry>0){
-                char v = carry+'0';
-                ans=v+ans;
-            }
-        }
-        else if(n2>=0){
-            while(n2>=0){
-                int d2 = num2[n2]-'0'+ carry;
-                int z=d2%10;
-                char s = '0'+z;
-                ans=s+ans;
-                carry=d2/10;
-                n2--;
-            }
-            if(carry>0){
-                char vt = carry+'0';
-                ans=vt+ans;
-            }
+        if(carry > 0){
+            c = '0' + carry ;
+            ans = c+ ans;
         }
         return ans;
     }
